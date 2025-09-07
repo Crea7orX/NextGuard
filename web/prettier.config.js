e065ba8d4069335e5ce945167d6b36c7ca30a382
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-09-07 22:34:15 +0300

    chore(app): Init web project

---
+++ web/prettier.config.js
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
