92e67de6554df8047d108c648b0eca61424e1327
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-09-11 02:21:52 +0300

    chore(app): Add svgr dev package

--- web/next.config.js
+++ web/next.config.js
@@ -5,6 +5,23 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/, // apply only to .js/.ts/.jsx/.tsx files
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+};
 
 export default config;
