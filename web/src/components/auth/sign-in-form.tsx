bf9e7ff58aa00d66872850cfe046ea9c08d01185
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-09-14 02:48:28 +0300

    feat(web): Implement email sign up logic

--- web/src/components/auth/sign-in-form.tsx
+++ web/src/components/auth/sign-in-form.tsx
@@ -1,29 +1,70 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Eye, EyeOff, LoaderCircle } from "lucide-react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import React from "react";
+import { useForm } from "react-hook-form";
 import GoogleIcon from "~/assets/icons/google.svg";
 import { ErrorAlert } from "~/components/auth/error-alert";
 import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import { authClient } from "~/lib/auth-client";
+import { cn } from "~/lib/utils";
+import { signInSchema, type SignIn } from "~/lib/validation/auth/sign-in";
 
 export function SignInForm({
   className,
   ...props
-}: React.ComponentProps<"form">) {
+}: React.ComponentProps<"div">) {
   const searchParams = useSearchParams();
   const redirectUrl = React.useMemo(
-    () => searchParams.get("redirect_url") ?? "/",
+    () => searchParams.get("redirect_url") ?? "/dashboard",
     [searchParams],
   );
 
   const [error, setError] = React.useState<string | null>(null);
+  const [isLoading, setIsLoading] = React.useState(false);
   const [showPassword, setShowPassword] = React.useState(false);
   const [isLoadingGoogle, setIsLoadingGoogle] = React.useState(false);
+
+  const form = useForm<SignIn>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: true,
+    },
+    disabled: isLoading || isLoadingGoogle,
+  });
+
+  function onSubmit(data: SignIn) {
+    void authClient.signIn.email(
+      {
+        ...data,
+        callbackURL: redirectUrl,
+      },
+      {
+        onRequest: () => {
+          setIsLoading(true);
+          setError(null);
+        },
+        onError: (ctx) => {
+          setIsLoading(false);
+          setError(ctx.error.message);
+        },
+      },
+    );
+  }
 
   function googleSignIn() {
     void authClient.signIn.social(
@@ -45,13 +86,13 @@ export function SignInForm({
   }
 
   return (
-    <form className={className} {...props}>
-      <div className="grid gap-6">
+    <Form {...form}>
+      <div className={cn("grid gap-6", className)} {...props}>
         {error && <ErrorAlert error={error} />}
         <Button
           variant="outline"
           className="w-full"
-          type="button"
+          disabled={form.formState.disabled}
           onClick={googleSignIn}
         >
           {isLoadingGoogle ? (
@@ -66,52 +107,82 @@ export function SignInForm({
             Or continue with
           </span>
         </div>
-        <div className="grid gap-6">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-6">
           <div className="grid gap-3">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email address"
+                      autoComplete="email webauthn"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
           </div>
           <div className="grid gap-3">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <a
-                href="#"
-                className="text-muted-foreground ml-auto text-sm underline-offset-4 hover:underline"
-              >
-                Forgot your password?
-              </a>
-            </div>
-            <div className="relative">
-              <Input
-                id="password"
-                type={showPassword ? "text" : "password"}
-                className="pr-10"
-                required
-              />
-              <Button
-                type="button"
-                variant="ghost"
-                size="sm"
-                className="absolute top-0.5 right-0.5 hover:bg-transparent"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? (
-                  <EyeOff className="h-4 w-4" />
-                ) : (
-                  <Eye className="h-4 w-4" />
-                )}
-              </Button>
-            </div>
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex items-center">
+                    <FormLabel>Password</FormLabel>
+                    <a
+                      href="#"
+                      className="text-muted-foreground ml-auto text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <FormControl>
+                    <div className="group relative">
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Enter your password"
+                        autoComplete="password"
+                        className="pr-10"
+                        {...field}
+                      />
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        size="sm"
+                        className="absolute top-0.5 right-0.5 opacity-0 transition-opacity group-hover:opacity-100 hover:bg-transparent"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? (
+                          <EyeOff className="h-4 w-4" />
+                        ) : (
+                          <Eye className="h-4 w-4" />
+                        )}
+                      </Button>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
-          <Button type="submit" className="w-full">
-            Sign In
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={form.formState.disabled}
+          >
+            {isLoading ? (
+              <LoaderCircle size={16} className="animate-spin" />
+            ) : (
+              <span>Sign In</span>
+            )}
           </Button>
-        </div>
+        </form>
         <div className="text-center text-sm">
           Don&apos;t have an account?{" "}
           <Link
@@ -122,6 +193,6 @@ export function SignInForm({
           </Link>
         </div>
       </div>
-    </form>
+    </Form>
   );
 }
