8ab1358e6e330c06b4879b8e5521753b190c286d
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-09-19 18:31:29 +0000

    style(web): Set no shring in error alert component

--- web/src/components/auth/error-alert.tsx
+++ web/src/components/auth/error-alert.tsx
@@ -7,7 +7,7 @@ interface Props {
 function ErrorAlert({ error }: Props) {
   return (
     <div className="bg-destructive/5 border-destructive text-destructive flex items-center gap-4 rounded-md border p-4">
-      <TriangleAlert className="size-6" />
+      <TriangleAlert className="size-6 shrink-0" />
       <p className="text-sm">{error}</p>
     </div>
   );
