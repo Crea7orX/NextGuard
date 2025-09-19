a6d14ef4b50c366760fc0faabc103e1bc2797fe6
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-09-19 09:27:44 +0000

    refactor(web): Remove card description on sign in page

--- web/src/app/auth/sign-in/page.tsx
+++ web/src/app/auth/sign-in/page.tsx
@@ -1,18 +1,11 @@
 import { SignInForm } from "~/components/auth/sign-in-form";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 
 export default function SignInPage() {
   return (
     <Card>
       <CardHeader className="text-center">
         <CardTitle className="text-xl">Welcome back</CardTitle>
-        <CardDescription>Login with your Google account</CardDescription>
       </CardHeader>
       <CardContent>
         <SignInForm />
