3790171a8b799ecdcf7f91ac938f6cf99a3497e3
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-09-19 18:34:28 +0000

    feat(web): Create password reset form component

---
+++ web/src/components/auth/password-reset-form.tsx
@@ -0,0 +1,241 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff, LoaderCircle, Lock } from "lucide-react";
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import GoogleIcon from "~/assets/icons/google.svg";
+import { ErrorAlert } from "~/components/auth/error-alert";
+import { SuccessAlert } from "~/components/auth/success-alert";
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { authClient } from "~/lib/auth-client";
+import { cn } from "~/lib/utils";
+import {
+  resetPasswordSchema,
+  type ResetPassword,
+} from "~/lib/validation/auth/reset-password";
+
+export function PasswordResetForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const redirectUrl = React.useMemo(
+    () => searchParams.get("redirect_url") ?? "/dashboard",
+    [searchParams],
+  );
+
+  const redirectSearchParams = React.useMemo(() => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete("token");
+    params.delete("error");
+    return params.toString();
+  }, [searchParams]);
+
+  React.useEffect(() => {
+    if (!searchParams.get("token"))
+      void router.push(`/auth/sign-in?${redirectSearchParams}`);
+    if (searchParams.get("error") === "INVALID_TOKEN") {
+      setError("Invalid or expired token");
+      return;
+    }
+    setDisabled(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [success, setSuccess] = React.useState<string | null>(null);
+  const [error, setError] = React.useState<string | null>(null);
+  const [disabled, setDisabled] = React.useState(true);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [isLoadingGoogle, setIsLoadingGoogle] = React.useState(false);
+
+  const form = useForm<ResetPassword>({
+    resolver: zodResolver(resetPasswordSchema),
+    defaultValues: {
+      password: "",
+      passwordConfirmation: "",
+    },
+    disabled: disabled || isLoading,
+  });
+
+  function onSubmit(data: ResetPassword) {
+    void authClient.resetPassword(
+      {
+        newPassword: data.password,
+        token: searchParams.get("token") ?? "",
+      },
+      {
+        onRequest: () => {
+          setIsLoading(true);
+          setSuccess(null);
+          setError(null);
+        },
+        onSuccess: () => {
+          setSuccess(
+            "Password reset successfully. Now you can sign in with your new password. You will be redirected to the sign in page after 5 seconds.",
+          );
+          setTimeout(() => {
+            void router.push(`/auth/sign-in?${redirectSearchParams}`);
+          }, 5000);
+        },
+        onError: (ctx) => {
+          setIsLoading(false);
+          setError(ctx.error.message);
+        },
+      },
+    );
+  }
+
+  function googleSignIn() {
+    void authClient.signIn.social(
+      {
+        provider: "google",
+        callbackURL: redirectUrl,
+      },
+      {
+        onRequest: () => {
+          setIsLoadingGoogle(true);
+          setError(null);
+        },
+        onError: (ctx) => {
+          setIsLoadingGoogle(false);
+          setError(ctx.error.message);
+        },
+      },
+    );
+  }
+
+  return (
+    <Form {...form}>
+      <div className={cn("grid gap-6", className)} {...props}>
+        {success && <SuccessAlert success={success} />}
+        {error && <ErrorAlert error={error} />}
+        <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-6">
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>New password</FormLabel>
+                <FormControl>
+                  <div className="group relative">
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Enter your new password"
+                      autoComplete="new-password"
+                      className="pr-10"
+                      {...field}
+                    />
+                    <Button
+                      type="button"
+                      variant="ghost"
+                      size="sm"
+                      className="absolute top-0.5 right-0.5 opacity-0 transition-opacity group-hover:opacity-100 hover:bg-transparent"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? (
+                        <EyeOff className="h-4 w-4" />
+                      ) : (
+                        <Eye className="h-4 w-4" />
+                      )}
+                    </Button>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirmation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm new password</FormLabel>
+                <FormControl>
+                  <div className="group relative">
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Confirm your new password"
+                      autoComplete="new-password"
+                      className="pr-10"
+                      {...field}
+                    />
+                    <Button
+                      type="button"
+                      variant="ghost"
+                      size="sm"
+                      className="absolute top-0.5 right-0.5 opacity-0 transition-opacity group-hover:opacity-100 hover:bg-transparent"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? (
+                        <EyeOff className="h-4 w-4" />
+                      ) : (
+                        <Eye className="h-4 w-4" />
+                      )}
+                    </Button>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={form.formState.disabled}
+          >
+            {isLoading ? (
+              <LoaderCircle size={16} className="animate-spin" />
+            ) : (
+              <span>Reset password</span>
+            )}
+          </Button>
+        </form>
+        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+          <span className="bg-card text-muted-foreground relative z-10 px-2">
+            Or use another method
+          </span>
+        </div>
+        <div className="flex flex-col gap-3">
+          <Button
+            variant="outline"
+            className="w-full"
+            disabled={form.formState.disabled && !disabled}
+            onClick={googleSignIn}
+          >
+            {isLoadingGoogle ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              <GoogleIcon />
+            )}
+            Sign in with Google
+          </Button>
+          <Button
+            variant="outline"
+            className="w-full"
+            disabled={form.formState.disabled && !disabled}
+            asChild
+          >
+            <Link href={`/auth/sign-in?${redirectSearchParams}`}>
+              <Lock />
+              Sign in with your password
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </Form>
+  );
+}
