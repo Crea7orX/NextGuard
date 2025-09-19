fd1501861c6510723fdc9633394bccee4783609a
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-09-19 09:21:03 +0000

    feat(web): Add reset password expires in env variable

--- web/src/env.js
+++ web/src/env.js
@@ -10,6 +10,7 @@ export const env = createEnv({
     DATABASE_URL: z.string().url(),
     BETTER_AUTH_SECRET: z.string(),
     BETTER_AUTH_URL: z.string().url().default("http://localhost:3000"),
+    BETTER_AUTH_RESET_PASSWORD_EXPIRES_IN: z.coerce.number().default(300),
     GOOGLE_CLIENT_SECRET: z.string(),
     RESEND_API_KEY: z.string(),
     NODE_ENV: z
@@ -24,6 +25,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_BASE_URL: z.string().url().default("http://localhost:3000"),
     NEXT_PUBLIC_BETTER_AUTH_URL: z
       .string()
       .url()
@@ -39,8 +41,11 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    BETTER_AUTH_RESET_PASSWORD_EXPIRES_IN:
+      process.env.BETTER_AUTH_RESET_PASSWORD_EXPIRES_IN,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
     NODE_ENV: process.env.NODE_ENV,
