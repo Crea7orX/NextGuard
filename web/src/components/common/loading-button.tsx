0465ce9088cfd5562beac43f3689631ebf31b08b
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-09-22 07:01:48 +0000

    feat(web): Animate loading button component change between loading and normal state

--- web/src/components/common/loading-button.tsx
+++ web/src/components/common/loading-button.tsx
@@ -1,9 +1,10 @@
 import { LoaderCircle } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+import React from "react";
 import { Button } from "~/components/ui/button";
 
 interface Props extends React.ComponentProps<typeof Button> {
   isLoading?: boolean;
-  children: React.ReactNode;
 }
 
 export function LoadingButton({
@@ -12,13 +13,37 @@ export function LoadingButton({
   children,
   ...props
 }: Props) {
+  const initialLoading = React.useRef(true); // Use a ref to disable animation on initial render
+  React.useEffect(() => {
+    if (initialLoading.current) {
+      initialLoading.current = false;
+    }
+  }, []);
+
+  function getKey(children: React.ReactNode) {
+    if (React.isValidElement(children)) {
+      return children.key ?? "content";
+    }
+    return "content";
+  }
+
   return (
     <Button className={className} {...props}>
-      {isLoading ? (
-        <LoaderCircle className="size-4 animate-spin" />
-      ) : (
-        <span>{children}</span>
-      )}
+      <AnimatePresence mode="wait">
+        <motion.span
+          key={isLoading ? "loading" : getKey(children)}
+          initial={{ scale: initialLoading.current ? 1 : 0 }}
+          animate={{ scale: 1 }}
+          exit={{ scale: 0 }}
+          transition={{ duration: 0.15, ease: "easeOut" }}
+        >
+          {isLoading ? (
+            <LoaderCircle className="size-6 animate-spin" />
+          ) : (
+            children
+          )}
+        </motion.span>
+      </AnimatePresence>
     </Button>
   );
 }
