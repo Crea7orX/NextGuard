6c70ffad2b76527a99ef29eac0e80215c8010d5e
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-09-22 10:58:58 +0000

    refactor(web): Remove card description from sign up page

--- web/src/app/auth/(protected)/sign-up/page.tsx
+++ web/src/app/auth/(protected)/sign-up/page.tsx
@@ -1,18 +1,11 @@
 import { SignUpForm } from "~/components/auth/sign-up-form";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 
 export default function SignUpPage() {
   return (
     <Card>
       <CardHeader className="text-center">
         <CardTitle className="text-xl">Create your account</CardTitle>
-        <CardDescription>Register with your Google account</CardDescription>
       </CardHeader>
       <CardContent>
         <SignUpForm />
