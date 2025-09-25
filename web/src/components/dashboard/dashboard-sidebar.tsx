44b3977c4bdfb0c64379e3655e7f5d6a54f7fbe4
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-09-25 22:16:54 +0000

    feat(web): Create dashboard sidebar component

---
+++ web/src/components/dashboard/dashboard-sidebar.tsx
@@ -0,0 +1,21 @@
+import { SpaceSidebarSelector } from "~/components/spaces/space-sidebar-selector";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+} from "~/components/ui/sidebar";
+
+export function DashboardSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SpaceSidebarSelector />
+      </SidebarHeader>
+      <SidebarContent></SidebarContent>
+      <SidebarFooter></SidebarFooter>
+    </Sidebar>
+  );
+}
