9751fac4ba4a8def348a9a6e803a45156dde60d1
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-09-26 09:02:21 +0000

    feat(web): Add user nav to header component

--- web/src/components/dashboard/header.tsx
+++ web/src/components/dashboard/header.tsx
@@ -1,4 +1,5 @@
 import { SidebarTrigger } from "~/components/ui/sidebar";
+import { UserNav } from "~/components/user/user-nav";
 import { cn } from "~/lib/utils";
 
 export function Header({
@@ -15,7 +16,7 @@ export function Header({
     >
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
-        <div className="ml-auto flex items-center gap-2">Profile</div>
+        <UserNav triggerClassName="ml-auto" />
       </div>
     </header>
   );
