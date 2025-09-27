dbb1bf489349431dd6bcada541a0d1005063f425
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-09-27 18:57:16 +0000

    feat(web): Use account profile section and set height in account dialog component

--- web/src/components/account/account-dialog.tsx
+++ web/src/components/account/account-dialog.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import { AccountEmailSection } from "~/components/account/account-email-section";
 import { AccountPasswordSection } from "~/components/account/account-password-section";
-import { AvatarWithFallback } from "~/components/common/avatar-with-fallback";
-import { Button } from "~/components/ui/button";
+import { AccountProfileSection } from "~/components/account/account-profile-section";
 import {
   Dialog,
   DialogContent,
@@ -11,41 +8,18 @@ import {
   DialogTitle,
 } from "~/components/ui/dialog";
 import { Separator } from "~/components/ui/separator";
-import { authClient } from "~/lib/auth-client";
 
 export function AccountDialog({
   ...props
 }: React.ComponentProps<typeof Dialog>) {
-  const { data: session, isPending, error } = authClient.useSession();
-  const isSessionLoading = !session || isPending || !!error;
-
-  if (isSessionLoading) {
-    return null;
-  }
-
   return (
     <Dialog {...props}>
-      <DialogContent className="lg:min-w-3xl">
+      <DialogContent className="flex h-full max-h-[40rem] flex-col overflow-auto lg:min-w-3xl">
         <DialogHeader>
           <DialogTitle>Account details</DialogTitle>
         </DialogHeader>
         <Separator />
-        <div className="flex items-center gap-2">
-          <div className="w-52">
-            <p>Profile</p>
-          </div>
-          <div className="flex flex-1 items-center gap-4">
-            <AvatarWithFallback
-              image={session.user.image}
-              name={session.user.name}
-              className="size-12"
-            />
-            <p className="font-semibold">{session.user.name}</p>
-            <Button variant="outline" className="ml-auto">
-              Update profile
-            </Button>
-          </div>
-        </div>
+        <AccountProfileSection />
         <Separator />
         <AccountEmailSection />
         <Separator />
