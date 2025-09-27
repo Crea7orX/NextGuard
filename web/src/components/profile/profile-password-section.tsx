7a2bf39e300759af0cbc2e3832b33ffc9f2880be
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-09-27 00:16:14 +0000

    feat(web): Create profile password section component

---
+++ web/src/components/profile/profile-password-section.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { AnimatePresence, motion } from "motion/react";
+import React from "react";
+import useMeasure from "react-use-measure";
+import { ProfileUpdatePasswordCard } from "~/components/profile/profile-update-password-card";
+import { Button } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+
+export function ProfilePasswordSection({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const [ref, { height }] = useMeasure({ offsetSize: true });
+
+  const [isUpdating, setIsUpdating] = React.useState(false);
+
+  return (
+    <div className={cn("flex items-start gap-2", className)} {...props}>
+      <div className="w-52">
+        <p>Password</p>
+      </div>
+      <motion.div
+        className="flex-1 -translate-y-1"
+        animate={{ height: height || "auto" }}
+        transition={{ duration: 0.2, ease: "easeOut" }}
+      >
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.div
+            className="flex flex-1 items-center gap-4"
+            initial={{ opacity: [0, 1], scale: [0.95, 1] }}
+            animate={{ opacity: [0, 1], scale: [0.95, 1] }}
+            exit={{ opacity: [1, 0], scale: [1, 0.95] }}
+            transition={{ duration: 0.4, ease: "easeOut", times: [0.5, 1] }}
+            key={isUpdating ? "updating" : "default"}
+            ref={ref}
+          >
+            {!isUpdating ? (
+              <>
+                <p className="text-lg font-bold">••••••••••</p>
+                <Button
+                  variant="outline"
+                  className="ml-auto"
+                  onClick={() => setIsUpdating(true)}
+                >
+                  Update password
+                </Button>
+              </>
+            ) : (
+              <ProfileUpdatePasswordCard
+                className="w-full"
+                onClose={() => setIsUpdating(false)}
+              />
+            )}
+          </motion.div>
+        </AnimatePresence>
+      </motion.div>
+    </div>
+  );
+}
