7eaa4a158c3fc50c7a8ccd09564b07ce39182721
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-09-27 19:04:26 +0000

    feat(web): Add two letter option in avatar with fallback component

--- web/src/components/common/avatar-with-fallback.tsx
+++ web/src/components/common/avatar-with-fallback.tsx
@@ -5,18 +5,31 @@ import { cn } from "~/lib/utils";
 interface Props extends React.ComponentProps<typeof Avatar> {
   image?: string | null;
   name: string;
+  twoLetter?: boolean;
 }
 
 export function AvatarWithFallback({
   className,
   image,
   name,
+  twoLetter = false,
   ...props
 }: Props) {
+  const getInitials = () => {
+    if (!twoLetter) {
+      return name.charAt(0);
+    }
+
+    const nameParts = name.split(/\s+/);
+    const firstInitial = nameParts[0]?.charAt(0) ?? "";
+    const lastInitial = nameParts[nameParts.length - 1]?.charAt(0) ?? "";
+    return `${firstInitial}${lastInitial}`;
+  };
+
   return (
     <Avatar className={cn("size-6", className)} {...props}>
       <AvatarImage src={image ?? undefined} alt={name} />
-      <AvatarFallback>{name.charAt(0)}</AvatarFallback>
+      <AvatarFallback>{getInitials()}</AvatarFallback>
     </Avatar>
   );
 }
