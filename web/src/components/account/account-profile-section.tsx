8e290ec48a2fd0aa32c875204d67c9ecfd5035b1
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-09-28 13:31:28 +0000

    style(web): Make account profile section component responsive

--- web/src/components/account/account-profile-section.tsx
+++ web/src/components/account/account-profile-section.tsx
@@ -21,18 +21,20 @@ export function AccountProfileSection({
   const [isUpdating, setIsUpdating] = React.useState(false);
 
   return (
-    <div className={cn("flex items-start gap-2", className)} {...props}>
-      <div className="w-52 translate-y-3">
+    <div
+      className={cn("flex flex-col items-start gap-2 lg:flex-row", className)}
+      {...props}
+    >
+      <div className="lg:w-52 lg:translate-y-3">
         <p>Profile</p>
       </div>
       <motion.div
-        className="flex-1"
+        className="w-full lg:flex-1"
         animate={{ height: height || "auto" }}
         transition={{ duration: 0.2, ease: "easeOut" }}
       >
         <AnimatePresence mode="wait" initial={false}>
           <motion.div
-            className="flex flex-1 items-center gap-4"
             initial={{ opacity: [0, 1], scale: [0.95, 1] }}
             animate={{ opacity: [0, 1], scale: [0.95, 1] }}
             exit={{ opacity: [1, 0], scale: [1, 0.95] }}
@@ -40,30 +42,36 @@ export function AccountProfileSection({
             key={isUpdating ? "updating" : "default"}
             ref={ref}
           >
-            {isSessionLoading ? (
-              <>
-                <Skeleton className="size-12 rounded-full" />
-                <Skeleton className="h-4 w-52" />
-                <Skeleton className="ml-auto h-8 w-42" />
-              </>
-            ) : !isUpdating ? (
-              <>
-                <AvatarWithFallback
-                  image={session.user.image}
-                  name={session.user.name}
-                  twoLetter
-                  largeSize
-                  className="size-12"
-                />
-                <p className="font-semibold">{session.user.name}</p>
-                <Button
-                  variant="outline"
-                  className="ml-auto"
-                  onClick={() => setIsUpdating(true)}
-                >
-                  Update profile
-                </Button>
-              </>
+            {!isUpdating || isSessionLoading ? (
+              <div className="flex flex-1 items-center gap-2 max-lg:pl-2 lg:gap-4">
+                {isSessionLoading ? (
+                  <>
+                    <Skeleton className="size-12 rounded-full" />
+                    <Skeleton className="h-4 w-52" />
+                    <Skeleton className="ml-auto h-8 w-42" />
+                  </>
+                ) : (
+                  <>
+                    <AvatarWithFallback
+                      image={session.user.image}
+                      name={session.user.name}
+                      twoLetter
+                      largeSize
+                      className="size-10 sm:size-12"
+                    />
+                    <p className="truncate font-semibold">
+                      {session.user.name}
+                    </p>
+                    <Button
+                      variant="outline"
+                      className="ml-auto"
+                      onClick={() => setIsUpdating(true)}
+                    >
+                      Update profile
+                    </Button>
+                  </>
+                )}
+              </div>
             ) : (
               <AccountUpdateProfileCard
                 className="w-full"
