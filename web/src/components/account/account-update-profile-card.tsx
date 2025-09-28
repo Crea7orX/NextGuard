a746033cf72e696e6fa70aaa556d001a4725f322
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-09-28 13:36:39 +0000

    style(web): Make account update profile card component responsive

--- web/src/components/account/account-update-profile-card.tsx
+++ web/src/components/account/account-update-profile-card.tsx
@@ -113,7 +113,7 @@ export function AccountUpdateProfileCard({
                 </FormItem>
               )}
             />
-            <div className="grid grid-cols-2 items-start gap-3">
+            <div className="grid items-start gap-6 sm:grid-cols-2 sm:gap-3">
               <FormField
                 control={form.control}
                 name="firstName"
