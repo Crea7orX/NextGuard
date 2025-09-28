851fcfd872003ccc627abd524b374c63891b6ccf
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-09-28 15:14:31 +0000

    feat(web): Use animated container in account profile section component

--- web/src/components/account/account-profile-section.tsx
+++ web/src/components/account/account-profile-section.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { AnimatePresence, motion } from "motion/react";
 import React from "react";
-import useMeasure from "react-use-measure";
 import { AccountUpdateProfileCard } from "~/components/account/account-update-profile-card";
+import { AnimatedContainer } from "~/components/common/animated-container";
 import { AvatarWithFallback } from "~/components/common/avatar-with-fallback";
 import { Button } from "~/components/ui/button";
 import { Skeleton } from "~/components/ui/skeleton";
@@ -17,7 +16,6 @@ export function AccountProfileSection({
   const { data: session, isPending, error } = authClient.useSession();
   const isSessionLoading = !session || isPending || !!error;
 
-  const [ref, { height }] = useMeasure({ offsetSize: true });
   const [isUpdating, setIsUpdating] = React.useState(false);
 
   return (
@@ -28,61 +26,48 @@ export function AccountProfileSection({
       <div className="lg:w-52 lg:translate-y-3">
         <p>Profile</p>
       </div>
-      <motion.div
+      <AnimatedContainer
         className="w-full lg:flex-1"
-        animate={{ height: height || "auto" }}
-        transition={{ duration: 0.2, ease: "easeOut" }}
+        uniqueKey={isUpdating ? "updating" : "default"}
+        alwaysAvailable
       >
-        <AnimatePresence mode="wait" initial={false}>
-          <motion.div
-            initial={{ opacity: [0, 1], scale: [0.95, 1] }}
-            animate={{ opacity: [0, 1], scale: [0.95, 1] }}
-            exit={{ opacity: [1, 0], scale: [1, 0.95] }}
-            transition={{ duration: 0.4, ease: "easeOut", times: [0.5, 1] }}
-            key={isUpdating ? "updating" : "default"}
-            ref={ref}
-          >
-            {!isUpdating || isSessionLoading ? (
-              <div className="flex flex-1 items-center gap-2 max-lg:pl-2 lg:gap-4">
-                {isSessionLoading ? (
-                  <>
-                    <Skeleton className="size-12 rounded-full" />
-                    <Skeleton className="h-4 w-52" />
-                    <Skeleton className="ml-auto h-8 w-42" />
-                  </>
-                ) : (
-                  <>
-                    <AvatarWithFallback
-                      image={session.user.image}
-                      name={session.user.name}
-                      twoLetter
-                      largeSize
-                      className="size-10 sm:size-12"
-                    />
-                    <p className="truncate font-semibold">
-                      {session.user.name}
-                    </p>
-                    <Button
-                      variant="outline"
-                      className="ml-auto"
-                      onClick={() => setIsUpdating(true)}
-                    >
-                      Update profile
-                    </Button>
-                  </>
-                )}
-              </div>
+        {!isUpdating || isSessionLoading ? (
+          <div className="flex flex-1 items-center gap-2 max-lg:pl-2 lg:gap-4">
+            {isSessionLoading ? (
+              <>
+                <Skeleton className="size-12 rounded-full" />
+                <Skeleton className="h-4 w-52" />
+                <Skeleton className="ml-auto h-8 w-42" />
+              </>
             ) : (
-              <AccountUpdateProfileCard
-                className="w-full"
-                onClose={() => setIsUpdating(false)}
-                name={session.user.name}
-                image={session.user.image}
-              />
+              <>
+                <AvatarWithFallback
+                  image={session.user.image}
+                  name={session.user.name}
+                  twoLetter
+                  largeSize
+                  className="size-10 sm:size-12"
+                />
+                <p className="truncate font-semibold">{session.user.name}</p>
+                <Button
+                  variant="outline"
+                  className="ml-auto"
+                  onClick={() => setIsUpdating(true)}
+                >
+                  Update profile
+                </Button>
+              </>
             )}
-          </motion.div>
-        </AnimatePresence>
-      </motion.div>
+          </div>
+        ) : (
+          <AccountUpdateProfileCard
+            className="w-full"
+            onClose={() => setIsUpdating(false)}
+            name={session.user.name}
+            image={session.user.image}
+          />
+        )}
+      </AnimatedContainer>
     </div>
   );
 }
