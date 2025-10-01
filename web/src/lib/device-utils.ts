578ff22e364c0044d18d2a7c1c2ee32c832b5314
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-10-01 13:41:47 +0000

    feat(web): Create device utils

---
+++ web/src/lib/device-utils.ts
@@ -0,0 +1,16 @@
+import type { deviceTypeEnum } from "~/server/db/schemas/pending-devices";
+
+type DeviceTypeValue = (typeof deviceTypeEnum.enumValues)[number];
+
+enum DeviceTypeMapping {
+  "0x54" = "hub",
+}
+
+export function getDeviceTypeFromSerialId(serialId: string) {
+  const typeId = serialId.substring(16, 18);
+  console.log(serialId, typeId);
+  const deviceType =
+    DeviceTypeMapping[`0x${typeId}` as keyof typeof DeviceTypeMapping];
+
+  return deviceType as DeviceTypeValue | undefined;
+}
