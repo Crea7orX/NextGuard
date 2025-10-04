78186ac7b8327dee0ce543ca341fce2edd1be006
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-10-04 17:27:12 +0000

    feat(web): Create use adopt pending device mutation hook

---
+++ web/src/hooks/api/pending-devices/use-adopt-pending-device-mutation.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { type AxiosError } from "axios";
+import { axiosInstance } from "~/lib/axios";
+import type { SuccessResponse } from "~/lib/responses";
+import type { PendingDeviceResponse } from "~/lib/validation/pending-device";
+
+interface Props {
+  id: string;
+}
+
+export function useAdoptPendingDeviceMutation({ id }: Props) {
+  const queryClient = useQueryClient();
+
+  return useMutation<PendingDeviceResponse, AxiosError>({
+    mutationKey: ["PendingDevices", "Adopt"],
+    mutationFn: async () =>
+      (
+        await axiosInstance.post<SuccessResponse<PendingDeviceResponse>>(
+          `/pending_devices/${id}/adopt`,
+        )
+      ).data.data,
+    onSuccess: () =>
+      Promise.all([
+        queryClient.invalidateQueries({
+          queryKey: ["PendingDevices", "GetAll"],
+        }),
+        queryClient.invalidateQueries({
+          queryKey: ["PendingDevices", "Get", id],
+        }),
+      ]),
+  });
+}
