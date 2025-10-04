5d1f88e83126d6a9fb08858727a4b93bc30e57b1
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-10-04 10:26:22 +0000

    feat(web): Create pending devices search and paginated response schemas

--- web/src/lib/validation/pending-device.ts
+++ web/src/lib/validation/pending-device.ts
@@ -1,5 +1,16 @@
+import {
+  createSearchParamsCache,
+  parseAsArrayOf,
+  parseAsInteger,
+  parseAsString,
+  parseAsStringEnum,
+} from "nuqs/server";
 import { z } from "zod";
-import { deviceTypeEnum } from "~/server/db/schemas/pending-devices";
+import { getSortingStateParser } from "~/lib/parsers";
+import {
+  deviceTypeEnum,
+  pendingDeviceState,
+} from "~/server/db/schemas/pending-devices";
 
 export const pendingDeviceCreateSchema = z.object({
   serialId: z.uuid(),
@@ -11,9 +22,39 @@ export const pendingDeviceResponseSchema = z.object({
   id: z.string(),
   serialId: z.uuid(),
   type: z.enum(deviceTypeEnum.enumValues),
+  state: z.enum(pendingDeviceState.enumValues),
   ownerId: z.string(),
   createdAt: z.number(),
   updatedAt: z.number().optional(),
 });
 
 export type PendingDeviceResponse = z.infer<typeof pendingDeviceResponseSchema>;
+
+export const pendingDevicesSearchParamsCache = createSearchParamsCache({
+  page: parseAsInteger.withDefault(1),
+  perPage: parseAsInteger.withDefault(10),
+  sort: getSortingStateParser<PendingDeviceResponse>().withDefault([
+    { id: "state", desc: false },
+    { id: "createdAt", desc: true },
+  ]),
+  serialId: parseAsString.withDefault(""),
+  type: parseAsArrayOf(
+    parseAsStringEnum(deviceTypeEnum.enumValues),
+  ).withDefault([]),
+  state: parseAsArrayOf(
+    parseAsStringEnum(pendingDeviceState.enumValues),
+  ).withDefault([]),
+});
+
+export type PendingDeviceSearchParams = Awaited<
+  ReturnType<typeof pendingDevicesSearchParamsCache.parse>
+>;
+
+export const pendingDevicesPaginatedResponseSchema = z.object({
+  data: pendingDeviceResponseSchema.array(),
+  pageCount: z.number(),
+});
+
+export type PendingDevicesPaginatedResponse = z.infer<
+  typeof pendingDevicesPaginatedResponseSchema
+>;
