e372e0f055db81c549d2e7779f08c52bbebc3270
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-10-04 17:51:19 +0000

    refactor(web): Make select space prompt use empty components

--- web/src/components/dashboard/select-space-prompt.tsx
+++ web/src/components/dashboard/select-space-prompt.tsx
@@ -1,27 +1,33 @@
 import { Building } from "lucide-react";
 import { SpaceSelectDropdownMenu } from "~/components/spaces/space-select-dropdown-menu";
 import { Button } from "~/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "~/components/ui/empty";
 
 export function SelectSpacePrompt({
   className,
   ...props
-}: React.ComponentProps<"div">) {
+}: React.ComponentProps<typeof Empty>) {
   return (
-    <div className={className} {...props}>
-      <div className="flex flex-col items-center gap-4">
-        <div className="bg-muted text-muted-foreground inline-flex size-12 items-center justify-center rounded-md border p-2.5">
-          <Building className="size-6" />
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <h2 className="font-bold">No active space</h2>
-          <p className="text-muted-foreground text-sm">
-            Select a space to get started.
-          </p>
-        </div>
+    <Empty className={className} {...props}>
+      <EmptyHeader>
+        <EmptyMedia variant="icon" className="size-12">
+          <Building className="size-8" />
+        </EmptyMedia>
+        <EmptyTitle>No active space</EmptyTitle>
+        <EmptyDescription>Select a space to get started.</EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
         <SpaceSelectDropdownMenu sideOffset={8}>
           <Button>Select space</Button>
         </SpaceSelectDropdownMenu>
-      </div>
-    </div>
+      </EmptyContent>
+    </Empty>
   );
 }
