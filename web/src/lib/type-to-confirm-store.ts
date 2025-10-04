75302dab8e5293f7e575db091404eebf92393f1e
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-10-04 14:28:10 +0000

    feat(web): Create type to confirm store

---
+++ web/src/lib/type-to-confirm-store.ts
@@ -0,0 +1,81 @@
+"use client";
+
+import type React from "react";
+import { create } from "zustand";
+import type { FormResponseMessageProps } from "~/components/ui/form";
+
+export interface TypeToConfirmOptions {
+  title?: string;
+  description?: React.ReactNode;
+  confirmText?: string;
+  caseInensitive?: boolean;
+  message?: FormResponseMessageProps;
+  warning?: string;
+  isLoading?: boolean;
+  confirmButtonText?: string;
+  cancelButtonText?: string;
+  onConfirm?: () => void | Promise<void>;
+  onCancel?: () => void;
+  onClose?: () => void;
+}
+
+export interface TypeToConfirmState extends TypeToConfirmOptions {
+  open: boolean;
+}
+
+interface TypeToConfirmStore {
+  state: TypeToConfirmState;
+  resolve: ((value: boolean) => void) | null;
+  show: (options: TypeToConfirmOptions) => Promise<boolean>;
+  close: (success?: boolean) => void;
+  setMessage: (message?: FormResponseMessageProps) => void;
+  setIsLoading: (isLoading: boolean) => void;
+  _confirm: () => void | Promise<void>;
+  _cancel: () => void;
+}
+
+export const useAlertStore = create<TypeToConfirmStore>((set, get) => ({
+  state: { open: false },
+  resolve: null,
+  show: (options) =>
+    new Promise<boolean>((resolve) => {
+      set({
+        state: { open: true, ...options },
+        resolve,
+      });
+    }),
+  close: (success = false) => {
+    const { resolve, state } = get();
+    if (state.isLoading && !success) return;
+    set({ state: { ...state, open: false } });
+    state.onClose?.();
+    resolve?.(success);
+    set({ resolve: null });
+  },
+  setMessage: (message) => {
+    set({ state: { ...get().state, message } });
+  },
+  setIsLoading: (isLoading) => {
+    set({ state: { ...get().state, isLoading } });
+  },
+  _confirm: () => {
+    const { state } = get();
+    if (state.isLoading) return;
+    void state.onConfirm?.();
+  },
+  _cancel: () => {
+    const { close, state } = get();
+    if (state.isLoading) return;
+    state.onCancel?.();
+    close(false);
+  },
+}));
+
+export const typeToConfirm = {
+  show: (opts: TypeToConfirmOptions) => useAlertStore.getState().show(opts),
+  close: (success?: boolean) => useAlertStore.getState().close(success),
+  setMessage: (message?: FormResponseMessageProps) =>
+    useAlertStore.getState().setMessage(message),
+  setIsLoading: (isLoading: boolean) =>
+    useAlertStore.getState().setIsLoading(isLoading),
+};
