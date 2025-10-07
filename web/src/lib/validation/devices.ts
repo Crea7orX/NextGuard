8c53501cd8058cc021170ead3fd6b42b64452cb3
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-10-07 04:59:34 +0000

    feat(web): Create device update validation schema

--- web/src/lib/validation/devices.ts
+++ web/src/lib/validation/devices.ts
@@ -23,6 +23,18 @@ export const deviceResponseSchema = z.object({
 
 export type DeviceResponse = z.infer<typeof deviceResponseSchema>;
 
+export const deviceUpdateSchema = z.object({
+  name: z.string().min(2).max(255).optional(),
+  description: z
+    .string()
+    .min(2)
+    .max(255)
+    .or(z.literal("").transform(() => null))
+    .nullish(),
+});
+
+export type DeviceUpdate = z.infer<typeof deviceUpdateSchema>;
+
 export const devicesSearchParamsCache = createSearchParamsCache({
   page: parseAsInteger.withDefault(1),
   perPage: parseAsInteger.withDefault(10),
