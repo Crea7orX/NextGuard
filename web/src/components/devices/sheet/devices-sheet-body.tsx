e373784b6fd034d123b006965359d39e2ffa0fa6
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-10-07 05:02:51 +0000

    feat(web): Create devices sheet body component

---
+++ web/src/components/devices/sheet/devices-sheet-body.tsx
@@ -0,0 +1,33 @@
+import { DevicesInfoTab } from "~/components/devices/sheet/devices-info-tab";
+import { DevicesSettingsTab } from "~/components/devices/sheet/devices-settings-tab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import { useGetDeviceByIdQuery } from "~/hooks/api/devices/use-get-device-by-id-query";
+
+interface Props {
+  id: string;
+}
+
+export function DevicesSheetBody({ id }: Props) {
+  const { data, isLoading } = useGetDeviceByIdQuery({
+    id,
+    refetchInterval: 2000,
+  });
+
+  // TODO: add skeletons
+  if (!data || isLoading) return <div>Loading...</div>;
+
+  return (
+    <Tabs defaultValue="info" className="h-full">
+      <TabsList className="w-full gap-2">
+        <TabsTrigger value="info">Info</TabsTrigger>
+        <TabsTrigger value="settings">Settings</TabsTrigger>
+      </TabsList>
+      <TabsContent value="info" asChild>
+        <DevicesInfoTab device={data} />
+      </TabsContent>
+      <TabsContent value="settings" asChild>
+        <DevicesSettingsTab device={data} />
+      </TabsContent>
+    </Tabs>
+  );
+}
