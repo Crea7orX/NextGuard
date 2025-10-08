63ee1a08d4caf135f7bee7310670c7546efa4bdf
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-10-08 23:23:14 +0000

    feat(websocket): Create session handler

---
+++ websocket/src/handlers/session.ts
@@ -0,0 +1,67 @@
+import type { WebSocket as WSWebSocket } from "ws";
+import { sendRequest } from "~/lib/requests";
+import { nowSec, signB64, verifyP256 } from "~/lib/utils";
+import { sessionMessageSchema } from "~/lib/validation/messages";
+import {
+  createSession,
+  generateSessionKey,
+  validateTimestamp,
+} from "~/sessions";
+
+export async function handleSession(ws: WSWebSocket, data: any) {
+  const message = sessionMessageSchema.safeParse(data);
+  if (!message.success) {
+    throw new Error("Invalid message!");
+  }
+
+  const { device_id, ts, nonce, sig } = message.data;
+
+  if (!validateTimestamp(ts)) {
+    console.log("Invalid timestamp!");
+    throw new Error("Invalid timestamp!");
+  }
+
+  const publicKeyResponse = await sendRequest({
+    method: "GET",
+    path: `/devices/${device_id}/public_key`,
+  });
+
+  if (!publicKeyResponse.success) {
+    console.log("Invalid response!", publicKeyResponse.error);
+    throw new Error("Invalid response!");
+  }
+  // TODO: validate data response
+
+  const signedBytes = Buffer.concat([
+    Buffer.from(String(device_id)),
+    Buffer.from(String(ts)),
+    Buffer.from(nonce, "base64"),
+  ]);
+  const isValid = verifyP256(
+    publicKeyResponse.data.publicKeyPem,
+    signedBytes,
+    Buffer.from(sig, "base64"),
+  );
+
+  if (!isValid) {
+    throw new Error("Invalid signature!");
+  }
+
+  const { ikm, salt, info, sessionKey } = generateSessionKey();
+  const session = createSession(device_id, ws, sessionKey);
+
+  const response = {
+    type: "session_ack",
+    ts: nowSec(),
+    seq0: 1,
+    kdf: {
+      alg: "HKDF-SHA256",
+      info: info.toString(),
+      salt: salt.toString("base64"),
+    },
+    ikm: ikm.toString("base64"),
+  };
+
+  ws.send(JSON.stringify({ ...response, sig: signB64(response) }));
+  return session;
+}
