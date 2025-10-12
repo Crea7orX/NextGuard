0e2b9d78f666e23c4b5f8c56f7d3707c041d3394
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-10-12 19:41:52 +0000

    feat(websocket): Create telemetry handler

---
+++ websocket/src/handlers/telemetry.ts
@@ -0,0 +1,36 @@
+import type { WebSocket as WSWebSocket } from "ws";
+import { sendRequest } from "~/lib/requests";
+import { generateNonce, nowSec } from "~/lib/utils";
+import { deviceTelemetrySchema } from "~/lib/validation/devices";
+import type { Message } from "~/lib/validation/messages";
+import { generateMac, type Session } from "~/sessions";
+
+export async function handleTelemtry(
+  ws: WSWebSocket,
+  message: Message,
+  session: Session,
+) {
+  const payload = deviceTelemetrySchema.safeParse(message.payload);
+  if (!payload.success) {
+    throw new Error("Invalid message!");
+  }
+
+  const telemetryResponse = await sendRequest({
+    method: "PATCH",
+    path: `/devices/${session.deviceId}/telemetry`,
+    body: payload.data,
+  });
+  if (!telemetryResponse.success) {
+    throw new Error("Invalid response!");
+  }
+
+  const frame = {
+    type: "telemetry_ack",
+    seq: ++session.lastSeqOut,
+    ts: nowSec(),
+    nonce: generateNonce(),
+  };
+  ws.send(
+    JSON.stringify({ ...frame, mac: generateMac(session.sessionKey, frame) }),
+  );
+}
