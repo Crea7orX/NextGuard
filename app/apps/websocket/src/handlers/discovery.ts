9f55e0fb4a82f9910a82ddef12ac52b49ae70c8d
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-11-02 16:43:10 +0000

    feat(websocket): Create discovery handler

---
+++ app/apps/websocket/src/handlers/discovery.ts
@@ -0,0 +1,36 @@
+import { pendingDeviceDiscoverySchema } from "@repo/validations/websockets/pending-devices";
+import type { WebSocket as WSWebSocket } from "ws";
+import { sendRequest } from "~/lib/requests";
+import { generateNonce, nowSec } from "~/lib/utils";
+import type { Message } from "~/lib/validation/messages";
+import { generateMac, type Session } from "~/sessions";
+
+export async function handleDiscovery(
+  ws: WSWebSocket,
+  message: Message,
+  session: Session,
+) {
+  const payload = pendingDeviceDiscoverySchema.safeParse(message.payload);
+  if (!payload.success) {
+    throw new Error("Invalid message!");
+  }
+
+  const discoveryResponse = await sendRequest({
+    method: "POST",
+    path: `/pending_devices/${session.deviceId}/discovery`,
+    body: payload.data,
+  });
+  if (!discoveryResponse.success) {
+    throw new Error("Invalid response!");
+  }
+
+  const frame = {
+    type: "discovery_ack",
+    seq: ++session.lastSeqOut,
+    ts: nowSec(),
+    nonce: generateNonce(),
+  };
+  ws.send(
+    JSON.stringify({ ...frame, mac: generateMac(session.sessionKey, frame) }),
+  );
+}
