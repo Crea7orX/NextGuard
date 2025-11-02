fbc037775a94bac1150aeafc4a22369d75001a1c
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-11-02 13:37:58 +0000

    feat(app/validations): Create validations package

---
+++ app/packages/validations/src/websockets/pending-devices.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const pendingDeviceResponseSchema = z.object({
+  id: z.string(),
+  serialId: z.string(),
+  publicKeyPem: z.string(),
+});
+
+export type PendingDeviceResponseSchema = z.infer<
+  typeof pendingDeviceResponseSchema
+>;
+
+export const pendingDeviceIntroduceSchema = z.object({
+  publicKeyPem: z.string().refine(
+    (key) => {
+      return (
+        key.includes("-----BEGIN PUBLIC KEY-----") &&
+        key.includes("-----END PUBLIC KEY-----")
+      );
+    },
+    {
+      message: "Public key must be in valid PEM format",
+    },
+  ),
+});
+
+export type PendingDeviceIntroduceSchema = z.infer<
+  typeof pendingDeviceIntroduceSchema
+>;
