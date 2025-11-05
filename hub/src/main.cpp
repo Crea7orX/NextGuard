6c5f38d7abc8cac2b098c7747822f5b1463504fd
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-11-05 22:48:34 +0200

    feat(hub): Implement lora manager in main.cpp

--- hub/src/main.cpp
+++ hub/src/main.cpp
@@ -12,6 +12,7 @@
 #include <webserver/WebServer.h>
 #include <network/NetworkManager.h>
 #include <telemetry/Telemetry.h>
+#include <lora/LoRaManager.h>
 
 // Global instances
 Logger logger;
@@ -24,6 +25,8 @@ FactoryReset factoryReset;
 WebServerManager webServer;
 NetworkManager networkManager;
 Telemetry telemetry;
+NodeManager nodeManager;
+LoRaManager loraManager;
 
 void setup() {
     Serial.begin(SERIAL_BAUD_RATE);
@@ -97,9 +100,17 @@ void setup() {
     // Initialize secure messaging
     secureMsg.begin(&logger, &storage);
     
+    // Initialize Node Manager
+    logger.info("Initializing Node Manager...");
+    nodeManager.begin(&logger);
+
+    // Initialize LoRa Manager
+    logger.info("Initializing LoRa Manager...");
+    loraManager.begin(&logger, &nodeManager, &wsManager);
+    
     // Initialize WebSocket
     logger.info("Initializing WebSocket...");
-    wsManager.begin(&logger, &storage, &secureMsg);
+    wsManager.begin(&logger, &storage, &secureMsg, &loraManager);
     wsManager.setServer(SERVER_HOST, SERVER_PORT, WEBSOCKET_PATH, SERVER_USE_TLS);
     wsManager.connect();
     
@@ -128,6 +139,59 @@ void loop() {
     
     // Handle automatic telemetry sending
     telemetry.loop();
+    
+    // Handle LoRa communication
+    loraManager.process();
+    
+    // Handle serial commands for LoRa
+    if (Serial.available()) {
+        char cmd = Serial.read();
+        if (cmd == 'a' || cmd == 'A') {
+            loraManager.enableAdoptionMode();
+        } else if (cmd == 's' || cmd == 'S') {
+            // Send command to node
+            Serial.println("[HUB] Enter command (format: nodeIndex,command):");
+            Serial.println("[HUB] Example: 0,BLINK (use 'l' to list nodes)");
+            
+            String input = "";
+            unsigned long startTime = millis();
+            while (millis() - startTime < 10000) {  // 10 second timeout
+                if (Serial.available()) {
+                    char c = Serial.read();
+                    if (c == '\n' || c == '\r') {
+                        if (input.length() > 0) break;
+                    } else {
+                        input += c;
+                    }
+                }
+                yield();
+            }
+            
+            if (input.length() > 0) {
+                int commaPos = input.indexOf(',');
+                if (commaPos > 0) {
+                    String indexStr = input.substring(0, commaPos);
+                    String command = input.substring(commaPos + 1);
+                    
+                    int nodeIndex = indexStr.toInt();
+                    loraManager.sendCommandOld(nodeIndex, command.c_str());
+                } else {
+                    Serial.println("[HUB] Invalid format");
+                }
+            }
+        } else if (cmd == 'l' || cmd == 'L') {
+            loraManager.listActiveNodes();
+        } else if (cmd == 'd' || cmd == 'D') {
+            loraManager.listDiscoveredNodes();
+        } else if (cmd == 'h' || cmd == 'H' || cmd == '?') {
+            Serial.println("[HUB] LoRa Commands:");
+            Serial.println("  a - Enter adoption mode");
+            Serial.println("  s - Send command to node");
+            Serial.println("  l - List active nodes");
+            Serial.println("  d - List discovered nodes");
+            Serial.println("  h - Show this help");
+        }
+    }
     
     delay(LOOP_DELAY_MS);
 }
