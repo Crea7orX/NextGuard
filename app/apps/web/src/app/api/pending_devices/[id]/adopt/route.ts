628b4461b25e9bd9e8e543b879c6faff607040f1
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-11-19 19:26:22 +0000

    feat(web): Inform hubs on adoption of auto discovered devices (#7)

--- app/apps/web/src/app/api/pending_devices/[id]/adopt/route.ts
+++ app/apps/web/src/app/api/pending_devices/[id]/adopt/route.ts
@@ -1,15 +1,18 @@
+import { adoptionRequestSchema } from "@repo/validations/websockets/adoptions";
 import type { NextRequest } from "next/server";
 import { authenticateAndAuthorizeUser } from "~/lib/auth-utils";
-import { handleError, NotFoundError } from "~/lib/errors";
+import { ConflictError, handleError, NotFoundError } from "~/lib/errors";
+import { sendRequest } from "~/lib/requests";
 import { createSuccessResponse } from "~/lib/responses";
 import { pendingDeviceResponseSchema } from "~/lib/validation/pending-device";
+import { getDevicesByType } from "~/server/db/queries/devices";
 import { adoptPendingDevice } from "~/server/db/queries/pending-devices";
 
 interface Props {
   params: Promise<{ id: string }>;
 }
 
-// adopt auto discovered device
+// Adopt auto discovered device
 export async function POST(request: NextRequest, { params }: Props) {
   try {
     const authResult = await authenticateAndAuthorizeUser(
@@ -25,6 +28,23 @@ export async function POST(request: NextRequest, { params }: Props) {
       userId: authResult.user.id,
     });
     if (!pendingDevice) throw new NotFoundError();
+
+    const hubs = await getDevicesByType({
+      type: "hub",
+      ownerId: authResult.ownerId,
+    });
+    if (hubs.length === 0) throw new ConflictError();
+
+    hubs.forEach(async (hub) => {
+      const response = await sendRequest({
+        method: "POST",
+        path: "/adopt",
+        body: adoptionRequestSchema.parse({
+          serialId: hub.serialId,
+          nodeSerialId: pendingDevice.serialId,
+        }),
+      });
+    });
 
     return createSuccessResponse(
       pendingDeviceResponseSchema.parse(pendingDevice),
